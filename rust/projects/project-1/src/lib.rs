0f24ad74ba0dae3c9a37ae919752f4a214c1d52f
wangzizheng <james.wang0057@gmail.com>
2024-06-01 15:09:17 +0800

    implement KvStore struct

--- rust/projects/project-1/src/lib.rs
+++ rust/projects/project-1/src/lib.rs
@@ -1,1 +1,25 @@
-// just leave it empty for now
+// src/lib.rs
+
+pub struct KvStore {
+    store: std::collections::HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {
+            store: std::collections::HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        self.store.get(&key).cloned()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
+    }
+}
