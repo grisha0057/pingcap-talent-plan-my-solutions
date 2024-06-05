d47d1faceafc1739e7d131d9323be9707082f024
wangzizheng <james.wang0057@gmail.com>
2024-06-05 23:38:02 +0800

    some implements

--- rust/projects/project-1/src/bin/kvstore.rs
+++ rust/projects/project-1/src/bin/kvstore.rs
@@ -1,50 +1,51 @@
 use std::env;
 use kvs::KvStore;
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         eprintln!("Usage: kvs <COMMAND> [ARGS]");
-        std::process::exit(1);
+        exit(1);
     }
+
+    let mut store = KvStore::new();
 
     match args[1].as_str() {
         "set" => {
             if args.len() != 4 {
                 eprintln!("Usage: kvs set <KEY> <VALUE>");
-                std::process::exit(1);
+                exit(1);
             }
-            let mut store = KvStore::new();
             store.set(args[2].clone(), args[3].clone());
-            println!("unimplemented");
         }
         "get" => {
             if args.len() != 3 {
                 eprintln!("Usage: kvs get <KEY>");
-                std::process::exit(1);
+                exit(1);
             }
-            let store = KvStore::new();
             match store.get(args[2].clone()) {
                 Some(value) => println!("{}", value),
-                None => println!("unimplemented"),  // todo 改为用panic!
+                None => eprintln!("Key not found"),
             }
         }
         "rm" => {
             if args.len() != 3 {
                 eprintln!("Usage: kvs rm <KEY>");
-                std::process::exit(1);
+                exit(1);
             }
-            let mut store = KvStore::new();
-            store.remove(args[2].clone());
-            println!("unimplemented");
+            match store.remove(args[2].clone()) {
+                Some(_) => (),
+                None => eprintln!("Key not found"),
+            }
         }
         "-V" => {
-            println!("kvs version 0.1.0");
+            println!("kvs version {}", env!("CARGO_PKG_VERSION"));
         }
         _ => {
             eprintln!("Unknown command: {}", args[1]);
-            std::process::exit(1);
+            exit(1);
         }
     }
 }
